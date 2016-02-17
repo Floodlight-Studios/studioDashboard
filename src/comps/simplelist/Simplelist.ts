f5f1c8b57f429b2e018e50898f6dfb594c87eae5
root <born2net@gmail.com>
2016-02-17 11:34:58 -0800

    progress on simple list added pipe filter

--- src/comps/simplelist/Simplelist.ts
+++ src/comps/simplelist/Simplelist.ts
@@ -1,34 +1,79 @@
-import {Component, Input, Output, EventEmitter, ChangeDetectionStrategy} from 'angular2/core';
+import {
+    Component, Input, Output, EventEmitter, ChangeDetectionStrategy, ElementRef,
+    ViewContainerRef, ContentChildren, QueryList, ViewChildren, ViewChild, ContentChild
+} from 'angular2/core';
 import {COMMON_DIRECTIVES} from "angular2/src/common/common_directives";
 import {FilterPipe} from "../../pipes/FilterPipe";
+import {BrowserDomAdapter} from "angular2/src/platform/browser/browser_adapter";
 
 @Component({
     selector: 'SimpleList',
+    //moduleId: module['id'],
     templateUrl: '/src/comps/simplelist/SimpleList.html',
     styleUrls: ['../comps/simplelist/SimpleList.css'],
     directives: [COMMON_DIRECTIVES],
-    changeDetection: ChangeDetectionStrategy.OnPush,
-    pipes: [FilterPipe]
+    pipes: [FilterPipe],
+    changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class SimpleList {
 
     filterValue = '';
-    selectedIndex = -1;
+    selectedIndices = [];
     selectedItem = true;
-    @Input() list:any[];
-    @Input() content:((any)=>string);
-    @Output() hover: EventEmitter<any> = new EventEmitter();
-    @Output() current: EventEmitter<any> = new EventEmitter();
+    @Input()
+    list:any[];
+    @Input()
+    content:((any)=>string);
+    @Input()
+    contentId:((any)=>string);
+    @Output()
+    hover:EventEmitter<any> = new EventEmitter();
+    @Output()
+    current:EventEmitter<any> = new EventEmitter();
+    // @ViewChildren('ul')
+    // listItems:QueryList<any>;
+    // ngAfterViewChecked(){
+    //     console.log(this.listItems);
+    // }
 
-    constructor() {
+    @ViewChildren('simpleItem')
+    simpleItems:QueryList<any>;
+
+    ngAfterViewChecked() {
+        //console.log(this.simpleItems);
     }
 
-    private itemSelected(item, index){
+    private el:HTMLElement;
+
+    constructor(private viewContainer:ViewContainerRef) {
+        this.el = viewContainer.element.nativeElement;
+    }
+
+    private itemSelected(item, index) {
         this.current.next(item);
-        this.selectedIndex = index;
+        if (this.selectedIndices.indexOf(index) > -1)
+            return this.selectedIndices = _.without(this.selectedIndices, index);
+        this.selectedIndices.push(index);
     }
 
-    getContent(item):string {
+    private onSelectAll() {
+        var self = this;
+        this.simpleItems.map(i=> {
+            self.selectedIndices.push(Number(i._appElement.nativeElement.id.split(':')[2]));
+        });
+    }
+
+    public getSelected() {
+
+    }
+
+    public getContentId(item):string {
+        if (!this.contentId)
+            return '-1';
+        return this.contentId(item);
+    }
+
+    public getContent(item):string {
         if (this.content) {
             return this.content(item);
         } else {
