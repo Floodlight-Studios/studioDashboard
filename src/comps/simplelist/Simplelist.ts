6cef24ce8076de105b015b30731813a359493958
root <born2net@gmail.com>
2016-02-17 15:29:51 -0800

    progress on simple list added pipe filter

--- src/comps/simplelist/Simplelist.ts
+++ src/comps/simplelist/Simplelist.ts
@@ -1,14 +1,15 @@
 import {
-    Component, Input, Output, EventEmitter, ChangeDetectionStrategy, ElementRef,
-    ViewContainerRef, ContentChildren, QueryList, ViewChildren, ViewChild, ContentChild
+    Component,
+    Input,
+    Output,
+    EventEmitter,
+    ChangeDetectionStrategy,
 } from 'angular2/core';
 import {COMMON_DIRECTIVES} from "angular2/src/common/common_directives";
 import {FilterPipe} from "../../pipes/FilterPipe";
-import {BrowserDomAdapter} from "angular2/src/platform/browser/browser_adapter";
 
 @Component({
     selector: 'SimpleList',
-    //moduleId: module['id'],
     templateUrl: '/src/comps/simplelist/SimpleList.html',
     styleUrls: ['../comps/simplelist/SimpleList.css'],
     directives: [COMMON_DIRECTIVES],
@@ -17,10 +18,8 @@ import {BrowserDomAdapter} from "angular2/src/platform/browser/browser_adapter";
 })
 export class SimpleList {
 
-    private filterValue = '';
-    private selectedIndices = [];
-    private selectedItem = true;
-    private el:HTMLElement;
+    private filter = '';
+    private _metadata:Object = {};
 
     @Input()
     list:any[];
@@ -32,39 +31,35 @@ export class SimpleList {
     hover:EventEmitter<any> = new EventEmitter();
     @Output()
     current:EventEmitter<any> = new EventEmitter();
-    @ViewChildren('simpleItem')
-    simpleItems:QueryList<any>;
-
-    constructor(private viewContainer:ViewContainerRef) {
-        this.el = viewContainer.element.nativeElement;
-    }
-
-    private setData(item, index){
-        console.log(item + ' ' + index);
-    }
 
     private itemSelected(item, index) {
-        this.current.next(item);
-        if (this.selectedIndices.indexOf(index) > -1)
-            return this.selectedIndices = _.without(this.selectedIndices, index);
-        this.selectedIndices.push(index);
+        let id = this.contentId ? this.contentId(item) : index;
+        this._metadata[id] = {
+            selected: !this._metadata[id].selected
+        };
+        this.current.next({id, selected: this._metadata[id].selected});
     }
 
-    private onSelectAll() {
-        var self = this;
-        this.simpleItems.map(i=> {
-            self.selectedIndices.push(Number(i._appElement.nativeElement.id.split(':')[2]));
-        });
+    private itemAllSelected() {
+        for (let id in this._metadata) {
+            this._metadata[id].selected = true;
+            this.current.next({id, selected: this._metadata[id].selected});
+        }
     }
 
-    public getSelected() {
-
+    private getMetadata(index, item) {
+        let id = this.contentId ? this.contentId(item) : index;
+        return this._metadata[id];
     }
 
-    public getContentId(item):string {
-        if (!this.contentId)
-            return '-1';
-        return this.contentId(item);
+    public getContentId(item, index):string {
+        let id = this.contentId ? this.contentId(item) : index;
+        if (this._metadata[id])
+            return id;
+        this._metadata[id] = {
+            selected: false
+        };
+        return id;
     }
 
     public getContent(item):string {
