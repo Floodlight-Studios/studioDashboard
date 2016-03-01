d028bfbe52b35f6cb2908a00a272a0c40d843161
root <born2net@gmail.com>
2016-02-29 16:15:12 -0800

    milestone

--- src/comps/app1/users/UserInfo.ts
+++ src/comps/app1/users/UserInfo.ts
@@ -4,18 +4,16 @@ import {List} from 'immutable';
 
 @Component({
     selector: 'UserInfo',
-    template: `
-        <span style="font-size: 6em; color: grey" class="fa fa-user"></span>
-        <h1>User Info</h1>
-    `,
+    templateUrl: `/src/comps/app1/users/UserInfo.html`,
     changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class UserInfo {
+    businessId:string;
+    
     @Input()
     set user(i_user:List<BusinessModel>){
-        console.log(i_user.first().getKey('businessId'));
+        this.businessId = i_user.first().getKey('businessId');
     }
-
 
     @Output()
     addToCart:EventEmitter<any> = new EventEmitter();
