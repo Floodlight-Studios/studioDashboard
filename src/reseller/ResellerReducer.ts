8f99d12f2dde0975d470e41a0ca1a2388de6a16f
root <born2net@gmail.com>
2016-03-21 15:36:53 -0700

    priv milestone 2

--- src/reseller/ResellerReducer.ts
+++ src/reseller/ResellerReducer.ts
@@ -26,7 +26,7 @@ export function reseller(state:Map<string,any> = Map<string,any>(), action:any):
                         if (group.get('tableName') == action.payload.tableName) {
                             var key = Lib.GetKeyFromMapIndex(group.get('columns'), action.payload.index);
                             var path = ['groups', c, 'columns', key];
-                            var data = i_privelegesModel.getData().updateIn(path, v=> action.payload.updTotalBits)
+                            var data = i_privelegesModel.getData().updateIn(path, v => action.payload.updTotalBits)
                             var updPriv = i_privelegesModel.setData<PrivelegesModel>(PrivelegesModel, data);
                             privileges = privileges.set(counter, updPriv);
                         }
