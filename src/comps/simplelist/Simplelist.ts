0b7ee9d2feb15dd0d24e97179f63006f698b69d4
root <born2net@gmail.com>
2016-03-22 13:24:19 -0700

    selected icon in simplelist

--- src/comps/simplelist/Simplelist.ts
+++ src/comps/simplelist/Simplelist.ts
@@ -21,8 +21,12 @@ export interface  ISimpleListItem {
 export class SimpleList {
 
     private filter = '';
-    private _metadata:Object = {};
-    private _editClickPending = false;
+    private m_icon = '';
+    private m_iconSelected:string = '';
+    private m_iconSelectedIndex:number = -1;
+    private m_iconSelectedMode:boolean = false;
+    private m_metadata:Object = {};
+    private m_editClickPending = false;
     @Input()
     list:List<any>;
     @Input()
@@ -32,52 +36,71 @@ export class SimpleList {
     @Input()
     contentId:((any)=>string);
     @Input()
-    icon:string;
+    set icon(i_icon:string) {
+        this.m_icon = i_icon;
+    }
     @Output()
     hover:EventEmitter<any> = new EventEmitter();
     @Output()
     iconClicked:EventEmitter<any> = new EventEmitter();
     @Output()
     selected:EventEmitter<any> = new EventEmitter();
+    @Input()
+    set iconSelectiondMode(mode:boolean) {
+        if (mode){
+            this.m_iconSelectedMode = true;
+            this.m_icon = 'fa-circle-o'
+            this.m_iconSelected = 'fa-circle'
+        }
+    }
 
     private itemSelected(item, index) {
         let id = this.contentId ? this.contentId(item) : index;
         if (!this.multi) {
-            for (let id in this._metadata) {
-                this._metadata[id] = {
+            for (let id in this.m_metadata) {
+                this.m_metadata[id] = {
                     selected: false
                 }
             }
         }
-        this._metadata[id] = {
+        this.m_metadata[id] = {
             item: item,
             index: index,
-            selected: this._editClickPending ? true : !this._metadata[id].selected
+            selected: this.m_editClickPending ? true : !this.m_metadata[id].selected
 
         }
-        if (this._editClickPending) {
-            this._editClickPending = false;
+        if (this.m_editClickPending) {
+            this.m_editClickPending = false;
             return;
         }
-        //this.current.next({item, selected: this._metadata[id].selected});
-        this.selected.next(this._metadata);
+        //this.current.next({item, selected: this.m_metadata[id].selected});
+        this.selected.next(this.m_metadata);
 
     }
 
+    private renderIcon(index, item) {
+        if (!this.m_iconSelectedMode)
+            return this.m_icon;
+        if (index==this.m_iconSelectedIndex)
+            return this.m_iconSelected;
+        return this.m_icon;
+    }
+
     private itemAllSelected() {
-        for (let id in this._metadata)
-            this._metadata[id].selected = true;
+        for (let id in this.m_metadata)
+            this.m_metadata[id].selected = true;
         this.list.forEach((i_item)=> {
             //this.current.next({item: i_item, selected: true});
-            this.selected.next(this._metadata);
+            this.selected.next(this.m_metadata);
         })
     }
 
     private onIconClick(event, index) {
         var self = this;
-        this._editClickPending = true;
+        this.m_editClickPending = true;
+        this.m_iconSelectedIndex = index;
         setTimeout(()=> {
-            let match = _.find(self._metadata, (i) => i.index == index);
+            let match = _.find(self.m_metadata, (i) => i.index == index);
             // console.log(match.item.getBusinessId() + ' ' + match.item.getKey('name'));
             this.iconClicked.next({
                 item: match,
@@ -85,19 +108,24 @@ export class SimpleList {
                 index: index
             });
         }, 1)
+        if (this.m_iconSelectedMode){
+            event.stopImmediatePropagation();
+            event.preventDefault();
+            return false;
+        }
         return true;
     }
 
     private getMetadata(index, item) {
         let id = this.contentId ? this.contentId(item) : index;
-        return this._metadata[id];
+        return this.m_metadata[id];
     }
 
     public getContentId(item, index):string {
         let id = this.contentId ? this.contentId(item) : index;
-        if (this._metadata[id])
+        if (this.m_metadata[id])
             return id;
-        this._metadata[id] = {
+        this.m_metadata[id] = {
             selected: false
         };
         return id;
@@ -112,7 +140,11 @@ export class SimpleList {
     }
 
     public getSelected() {
-        return this._metadata;
+        return this.m_metadata;
+    }
+
+    public setSelectedIcon(i_index){
+        this.m_iconSelectedIndex = i_index;
     }
 }
 
