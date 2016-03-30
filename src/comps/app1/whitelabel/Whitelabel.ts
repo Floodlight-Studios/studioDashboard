ea397986e4c4b72f739d068f2f1022ba25478931
root <born2net@gmail.com>
2016-03-30 10:55:46 -0700

    whitelabel form updates

--- src/comps/app1/whitelabel/Whitelabel.ts
+++ src/comps/app1/whitelabel/Whitelabel.ts
@@ -4,10 +4,14 @@ import {AuthService} from "../../../services/AuthService";
 import {appInjService} from "../../../services/AppInjService";
 import {Tab} from "../../tabs/tab";
 import {Tabs} from "../../tabs/tabs";
+import {WhitelabelModel} from "../../../reseller/WhitelabelModel";
+import {ResellerAction} from "../../../reseller/ResellerAction";
+import {AppStore} from "angular2-redux-util/dist/index";
+import {FORM_DIRECTIVES, ControlGroup, FormBuilder, AbstractControl, Validators} from "angular2/common";
 
 @Component({
     selector: 'whitelabel',
-    directives: [Tab, Tabs],
+    directives: [Tab, Tabs, FORM_DIRECTIVES],
     templateUrl: '/src/comps/app1/whitelabel/Whitelabel.html',
     changeDetection: ChangeDetectionStrategy.OnPush
 })
@@ -17,8 +21,39 @@ import {Tabs} from "../../tabs/tabs";
 })
 export class Whitelabel {
 
-    constructor() {
+    constructor(private appStore:AppStore, private fb:FormBuilder, private resellerAction:ResellerAction) {
+        var i_reseller = this.appStore.getState().reseller;
 
+        this.whitelabelModel = i_reseller.getIn(['whitelabel']);
+        this.unsub = this.appStore.sub((whitelabelModel:WhitelabelModel) => {
+            this.whitelabelModel = whitelabelModel;
+        }, 'reseller.whitelabel');
+
+        this.whitelabelForm = fb.group({
+            'companyName': ['', Validators.required],
+            'logoTooltip': ['', Validators.required],
+            'logoLink': ['', Validators.required],
+            'createAccountOption': ['', Validators.required]
+        });
+
+        this.whitelabelForm.valueChanges.debounceTime(1500).subscribe(
+            (value:string) => {
+                console.log('name changed, notified via observable: ', value);
+            }
+        );
+    }
+
+    private whitelabelForm:ControlGroup;
+    private whitelabelModel:WhitelabelModel;
+    private unsub;
+
+    // private companyName:AbstractControl;
+    // private logoTooltip:AbstractControl;
+    // this.companyName = this.whitelabelForm.controls['companyName'];
+    // this.logoTooltip = this.whitelabelForm.controls['logoTooltip'];
+
+    private ngOnDestroy() {
+        this.unsub();
     }
 }
 
