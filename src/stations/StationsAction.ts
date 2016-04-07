42c316527d8eb5892fd9aa7f34ff72e764db44b4
root <born2net@gmail.com>
2016-04-06 21:45:54 -0700

    sample on multiple url Observable

--- src/stations/StationsAction.ts
+++ src/stations/StationsAction.ts
@@ -1,8 +1,11 @@
 import {Injectable} from "angular2/core";
 import {Actions, AppStore} from "angular2-redux-util";
 import {Http, Jsonp} from "angular2/http";
+import {Observable} from "rxjs/Observable";
 import {List, Map} from 'immutable';
 import {StationModel} from "./StationModel";
+import {Subject} from "rxjs/Subject";
+
 
 export const RECEIVE_STATIONS = 'RECEIVE_STATIONS';
 
@@ -12,9 +15,67 @@ export class StationsAction extends Actions {
     constructor(private appStore:AppStore, private _http:Http, private jsonp:Jsonp) {
         super(appStore);
         this.m_parseString = require('xml2js').parseString;
+        // this.listenFetchBusinessUser();
     }
 
     private m_parseString;
+    // private businessesRequest$;
+    private unsub;
+
+
+    // demo
+    // var s = ['https://neptune.signage.me','https://earth.signage.me','https://moon.signage.me']
+    // this.appStore.dispatch(this.stationsAction.fetchBusinessUser(s));
+    // setTimeout(()=>{
+    // // this.appStore.dispatch(this.stationsAction.fetchBusinessUser([]));
+    // },100)
+
+    // demo
+    // private listenFetchBusinessUser() {
+    //     this.businessesRequest$ = new Subject();
+    //     this.unsub = this.businessesRequest$
+    //         .map(v=> {
+    //             return v;
+    //         })
+    //         .debounceTime(100)
+    //         .switchMap((values:{servers:Array<string>, dispatch:(value:any)=>any}):any => {
+    //             if (values.servers.length == 0)
+    //                 return 'CANCEL_PENDING_NET_CALLS';
+    //             var dispatch = values.dispatch;
+    //             // Observable.forkJoin
+    //             return values.servers.map(i_source => {
+    //                 var url:string = 'https://galaxy.signage.me/WebService/ResellerService.ashx?command=GetCustomers&resellerUserName=rs@ms.com&resellerPassword=rrr'
+    //                 return this._http.get(url)
+    //                     .map(result => {
+    //                         console.log(result);
+    //                     }).subscribe((res)=> {
+    //                     }, err=> {
+    //                         console.log(err);
+    //                     }, ()=> {
+    //                         console.log();
+    //                     });
+    //             })
+    //
+    //         }).share()
+    //         .subscribe();
+    // }
+
+    // demo
+    // public fetchBusinessUser(servers:Array<string>) {
+    //     Observable.forkJoin(
+    //         this._http.get('https://galaxy.signage.me/WebService/ResellerService.ashx?command=GetCustomers&resellerUserName=rs@ms.com&resellerPassword=rrr'),
+    //         this._http.get('https://galaxy.signage.me/WebService/ResellerService.ashx?command=GetCustomers&resellerUserName=rs@ms.com&resellerPassword=rrr')
+    //     ).subscribe(
+    //         data => {
+    //             console.log(data);
+    //         },
+    //         err => console.error(err)
+    //     );
+    //
+    //     return (dispatch) => {
+    //         this.businessesRequest$.next({servers: servers, dispatch: dispatch});
+    //     };
+    // }
 
     public getStationsInfo(i_source:string, i_businesses:Array<any>) {
         var self = this;
