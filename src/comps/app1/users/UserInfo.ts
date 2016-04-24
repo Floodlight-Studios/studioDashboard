cab251e9e4edc26e4f9509fd24b2a515e8cd7376
root <born2net@gmail.com>
2016-04-24 07:53:45 -0700

    working on user change pass

--- src/comps/app1/users/UserInfo.ts
+++ src/comps/app1/users/UserInfo.ts
@@ -1,65 +1,24 @@
-import {
-    Component, Input, Output, EventEmitter, ChangeDetectionStrategy, OnChanges, SimpleChange,
-    ChangeDetectorRef, ApplicationRef, NgZone
-} from 'angular2/core'
+import {Component, Input, ChangeDetectionStrategy, ViewChild, ChangeDetectorRef} from 'angular2/core'
 import {BusinessModel} from "../../../business/BusinessModel";
-import {List} from 'immutable';
 import {Infobox} from "../../infobox/Infobox";
 import {UserStorage} from "./UserStorage";
 import {InputEdit} from "../../inputedit/InputEdit";
-import {AppStore} from "angular2-redux-util/dist/index";
-import {BusinessUser} from "../../../business/BusinessUser";
-import {BusinessAction} from "../../../business/BusinessAction";
 import {ISimpleListItem} from "../../simplelist/Simplelist";
+import {ModalComponent} from "../../../comps/ng2-bs3-modal/components/modal";
+import {MODAL_DIRECTIVES, ModalResult} from "../../ng2-bs3-modal/ng2-bs3-modal";
+import {ChangePass} from "../../../comps/app1/users/ChangePass";
 
 @Component({
     selector: 'UserInfo',
-    directives: [Infobox, UserStorage, InputEdit],
+    directives: [Infobox, UserStorage, InputEdit, MODAL_DIRECTIVES, ChangePass],
     templateUrl: `/src/comps/app1/users/UserInfo.html`,
     changeDetection: ChangeDetectionStrategy.OnPush,
     styleUrls: [`../comps/app1/users/UserInfo.css`]
 
 })
 export class UserInfo {
-    businessId:string;
-    nameEmail;
-    serverStats = [];
-    serverStatsCategories = [];
-    stylesObj;
-    stylesDesc;
-    userName;
-    maxMonitors;
-    businessDescription;
-    lastLogin;
-    studioVersion;
-    studioIcon;
-    allowSharing = '';
-    accountStatus;
-    resellerId;
-    verifiedIcon;
-    fromTemplateId;
-    unsub;
 
-    @Input()
-    set user(i_simpleListItem:ISimpleListItem) {
-        var businessUser:BusinessModel = i_simpleListItem.item.item;
-        this.businessId = businessUser.getBusinessId();
-        this.userName = businessUser.getKey('name');
-        this.maxMonitors = businessUser.getKey('maxMonitors');
-        this.businessDescription = businessUser.getKey('businessDescription');
-        this.lastLogin = businessUser.getKey('lastLogin');
-        this.allowSharing = businessUser.getKey('allowSharing') == '0' ? '' : 'checked';
-        this.studioVersion = businessUser.getKey('studioLite') == 1 ? 'StudioLite' : 'StudioPro';
-        this.studioIcon = this.studioVersion == 'StudioLite' ? 'fa-circle-o' : 'fa-circle';
-        this.fromTemplateId = businessUser.getKey('fromTemplateId');
-        this.accountStatus = businessUser.getKey('accountStatus');
-        this.verifiedIcon = this.accountStatus == '2' ? 'fa-check' : 'fa-remove';
-        this.resellerId = businessUser.getKey('resellerId');
-
-        //this.appStore.dispatch(this.businessActions.fetchBusinessUser(this.businessId));
-    }
-
-    constructor(private appStore:AppStore, private businessActions:BusinessAction, private ref:ChangeDetectorRef) {
+    constructor(private ref:ChangeDetectorRef) {
         var w = '150px';
         this.stylesObj = {
             input: {
@@ -95,11 +54,58 @@ export class UserInfo {
         }
     };
 
+    businessId:string;
+    nameEmail;
+    serverStats = [];
+    serverStatsCategories = [];
+    stylesObj;
+    stylesDesc;
+    userName;
+    maxMonitors;
+    businessDescription;
+    lastLogin;
+    studioVersion;
+    studioIcon;
+    allowSharing = '';
+    accountStatus;
+    resellerId;
+    verifiedIcon;
+    fromTemplateId;
+    unsub;
+
+    @Input()
+    set user(i_simpleListItem:ISimpleListItem) {
+        var businessUser:BusinessModel = i_simpleListItem.item.item;
+        this.businessId = businessUser.getBusinessId();
+        this.userName = businessUser.getKey('name');
+        this.maxMonitors = businessUser.getKey('maxMonitors');
+        this.businessDescription = businessUser.getKey('businessDescription');
+        this.lastLogin = businessUser.getKey('lastLogin');
+        this.allowSharing = businessUser.getKey('allowSharing') == '0' ? '' : 'checked';
+        this.studioVersion = businessUser.getKey('studioLite') == 1 ? 'StudioLite' : 'StudioPro';
+        this.studioIcon = this.studioVersion == 'StudioLite' ? 'fa-circle-o' : 'fa-circle';
+        this.fromTemplateId = businessUser.getKey('fromTemplateId');
+        this.accountStatus = businessUser.getKey('accountStatus');
+        this.verifiedIcon = this.accountStatus == '2' ? 'fa-check' : 'fa-remove';
+        this.resellerId = businessUser.getKey('resellerId');
+    }
+
+
+    @ViewChild('modalChangePassword')
+    modalChangePassword:ModalComponent;
+
     updateUi() {
         try {
             this.ref.detectChanges();
         } catch (e) {
         }
+    }
+
+    private onPasswordChange() {
+        this.modalChangePassword.open('lg');
+    }
+
+    private onModalClose(result:ModalResult) {
     }
 
     ngAfterViewChecked() {
--- src/comps/app1/users/UsersDetails.ts
+++ src/comps/app1/users/UsersDetails.ts
@@ -143,3 +143,4 @@ export class UsersDetails {
         return Lib.GetAccessMask(accessMask);
     }
 }
+
